59670890e65b189b6db04fd7b25e4d28d0ae4f59
alona <aloyna.vasilieva@gmail.com>
2025-02-26 18:24:25 +0100

    added header

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let documentActions = (e) => {
+    const targetElement = e.target
+    if(targetElement.closest('.icon-menu')){
+        document.documentElement.classList.toggle('open-menu')
+    }
+}
+
+document.addEventListener('click', documentActions)
